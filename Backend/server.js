af34fb84962004830d218777009b1d37dc0db934
Vikas Talreja <vikazztalreja@gmail.com>
2024-12-02 12:53:06 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const app = require('./app');
+const server =  http.createServer(app);
+
+
+const port = 4000;
+
+
+server.listen(port, () => {
+    console.log(`running on ${port}`);
+});
+
