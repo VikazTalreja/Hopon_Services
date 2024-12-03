f286d8f561475b81fa6de89d3e908b25785ed202
Vikas Talreja <vikazztalreja@gmail.com>
2024-12-03 18:11:56 +0530

    Register User,Login and Profile Feature Updated

---
+++ Backend/models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+
+    fullname:{
+        firstname:{        
+            type:String,
+            required:true,
+            minlength:[3 , 'First letter must be at least 3 characters long']
+        },
+        lastname:{        
+            type:String,
+            required:false,
+            minlength:[3 , 'Last letter must be at least 3 characters long']
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength:[5 , " Email must be at least 3 characters Long"]
+    },
+    password:{
+        type:String,
+        required:true,
+        select:false
+    },
+
+    socketId:{
+        type:String
+    }
+})
+
+
+UserSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({_id: this._id} , process.env.JWT_SECRET);
+    return token;
+}
+
+UserSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password , this.password);
+}
+
+UserSchema.statics.hashpassword =async function(password){
+    return await bcrypt.hash(password , 10);
+}
+
+const userModel = mongoose.model( 'user' , UserSchema);
+
+module.exports = userModel;
---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const { body } = require('express-validator')
+const userController = require('../controllers/user.controller')
+const authMiddleware = require('../middleware/auth.middleware')
+
+router.post('/register' , [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({min: 3}).withMessage("Min length of Name Should be at least 3 Characters"),
+    body('password').isLength({min: 8 }).withMessage("password must be 8 characters long ")    
+],
+userController.registerUser
+)
+
+router.post('/login' , [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min: 8 }).withMessage("password must be 8 characters long ")    
+],
+userController.loginUser
+)
+
+router.get('/profile' ,authMiddleware.authUser , userController.getUserProfile)
+
+
+module.exports = router; 
