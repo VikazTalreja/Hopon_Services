fcbd6899422346657a79154a4e86ae70fef23846
Vikas Talreja <vikazztalreja@gmail.com>
2024-12-13 22:12:04 +0530

    UserLogin Updates

--- Frontend/src/pages/userLogin.jsx
+++ Frontend/src/pages/userLogin.jsx
@@ -9,7 +9,7 @@ const userLogin = () => {
      </div>
       <form>
         <h3 className="font-semibold text-lg mb-2">What your Email</h3>
-        <input className="bg-[#eeee] rounded-md px-4 py-2 border w-full text-lg placeholder:text-md " type="email" required="true" placeholder="email@gmail.com" />
+        <input className="bg-[#eeee] rounkkded-md px-4 py-2 border w-full text-lg placeholder:text-md " type="email" required="true" placeholder="email@gmail.com" />
         <h3 className="font-semibold text-lg mt-4 mb-2">Enter Password</h3>
         <input className="bg-[#eeee] rounded-md px-4 py-2 border w-full text-lg placeholder:text-md " type="password" required="true" placeholder="password" />
         <button className="flex items-center justify-center w-full bg-black text-white py-3 rounded-xl mt-5">Login</button>
